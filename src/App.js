9c15cb3a0211152914c6cfbb0e6218a336e5da03
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-02-09 15:48:16 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{Fragment} from 'react';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     
     <>
       <Header/>
+      <MealsSummary/>
 
     </>
   );
