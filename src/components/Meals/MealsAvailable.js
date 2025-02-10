f303be5b97f1fec1c7b4d79783600c1327573284
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-02-10 14:18:32 +0530

    Making the Items visible

--- src/components/Meals/MealsAvailable.js
+++ src/components/Meals/MealsAvailable.js
@@ -1,17 +1,25 @@
 import style from './MealsAvailable.module.css';
-
+import MealsItem from './MealItem/MealsItem';
+import Card from '../UI/Card';
 const MealsAvailable=()=>{
-    const mealsList=[{},
+    const mealsList=[
         {id:'m1',name:'Sushi',description:'Finest Fish and Vegies',price:22.99},
         {id:'m2',name:'Schnitzel',description:'German Speciality',price:16.5},
         {id:'m3',name:'Barbecue Burger',description:'American,raw,meaty',price:12.99},
         {id:'m4',name:'Green Bowl',description:'Healty...and green...',price:18.99}
     ]
     return(
-        <section>
-            <ul className={style.ul}>
+        <section className={style.meals}>
+            <Card>
+                <ul>
+                {mealsList.map(meal=><MealsItem 
+                key={meal.id} 
+                name={meal.name} 
+                description={meal.description} 
+                price={meal.price}/>)}
+                </ul>
 
-            </ul>
+            </Card>
         </section>
     );
 
